84588461899e5173b70346852fa83e839683060a
Vincent Kyle Caballa <vincentkyle27@gmail.com>
2021-01-10 22:42:27 +0800

    changes

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,19 +1,32 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static int SPLASH_TIME_OUT = 5000;
     private Button regbutton;
     private Button loginbutton;
+    private SpeechRecognizer speechRecognizer;
+    private EditText searchBar;
+    private ImageView voiceBtn;
 
 
     @Override
@@ -37,6 +50,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+        != PackageManager.PERMISSION_GRANTED) {
+            checkPermission();
+        }
+
+        searchBar = findViewById(R.id.text);
+        voiceBtn = findViewById(R.id.voiceBtn);
+
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+
+        final Intent speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
 
     }
 
@@ -49,6 +74,23 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, Homepage.class);
         startActivity(intent);
     }
+
+    private void checkPermission() {
+
+    }
+
+    public void onResults(Bundle bundle) {
+        voiceBtn.setImageResource(R.drawable.ic_mic);
+        ArrayList<String> data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+        searchBar.setText(0);
+    }
+
+    public void onBeginningOfSpeech() {
+        searchBar.setText("");
+        searchBar.setHint("Listening...");
+    }
+
+    
 
 }
 
