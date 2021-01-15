93868ae7cdbb9191b8964207378027086cd130ff
Vincent Kyle Caballa <vincentkyle27@gmail.com>
2021-01-15 20:59:32 +0800

    changes

---
+++ app/src/main/java/com/example/myapplication/HelperClasses/HomeAdapter/FeaturedAdapter.java
@@ -0,0 +1,69 @@
+package com.example.myapplication.HelperClasses.HomeAdapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.view.menu.MenuView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.R;
+
+import java.util.ArrayList;
+
+public class FeaturedAdapter extends RecyclerView.Adapter<FeaturedAdapter.AdapterViewHolder> {
+    
+    ArrayList<FeaturedHelper> featuredLocations;
+
+    public FeaturedAdapter(ArrayList<FeaturedHelper> featuredLocations) {
+        this.featuredLocations = featuredLocations;
+    }
+
+    @NonNull
+    @Override
+    public AdapterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.products_design_view, parent, false);
+        AdapterViewHolder adapterViewHolder = new AdapterViewHolder(v);
+        return adapterViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdapterViewHolder holder, int position) {
+        FeaturedHelper featuredHelper = featuredLocations.get(position);
+
+        holder.image.setImageResource(featuredHelper.getImg());
+        holder.description.setText(featuredHelper.getDesc());
+        holder.title.setText(featuredHelper.getTitle());
+        holder.price.setText(featuredHelper.getPrice());
+        holder.addtocart.setImageResource(featuredHelper.getAddtocart());
+    }
+
+    @Override
+    public int getItemCount() {
+        return featuredLocations.size();
+    }
+
+    public static class AdapterViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView image, addtocart;
+        TextView title, description, price;
+        Button buy;
+
+
+        public AdapterViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            //get design via Hooks
+            image = itemView.findViewById(R.id.productsImg);
+            price = itemView.findViewById(R.id.pricePhp);
+            title = itemView.findViewById(R.id.titleTxt);
+            description = itemView.findViewById(R.id.descTxt);
+            addtocart = itemView.findViewById(R.id.addtoCart);
+            buy = itemView.findViewById(R.id.buyBtn);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/HelperClasses/HomeAdapter/FeaturedHelper.java
@@ -0,0 +1,35 @@
+package com.example.myapplication.HelperClasses.HomeAdapter;
+
+public class FeaturedHelper {
+
+    int img, addtocart;
+    String title, desc, price;
+
+    public FeaturedHelper(int img, int addtocart, String title, String desc, String price) {
+        this.img = img;
+        this.title = title;
+        this.desc = desc;
+        this.addtocart = addtocart;
+        this.price = price;
+    }
+
+    public int getImg() {
+        return img;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public int getAddtocart() {
+        return addtocart;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+}
