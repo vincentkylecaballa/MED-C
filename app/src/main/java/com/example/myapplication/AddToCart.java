e41f64d14e2f4216e2797c4a62bef5db95a284fb
Vincent Kyle Caballa <vincentkyle27@gmail.com>
2021-01-20 20:56:38 +0800

    changes

---
+++ app/src/main/java/com/example/myapplication/AddToCart.java
@@ -0,0 +1,37 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class AddToCart extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener  {
+
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_to_cart);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        bottomNavigationView.postDelayed(() -> {
+            int itemId = menuItem.getItemId();
+            if (itemId == R.id.home) {
+                startActivity(new Intent(this, Homepage.class));
+            } else if (itemId == R.id.addtoCart) {
+                startActivity(new Intent(this, AddToCart.class));
+            } else if (itemId == R.id.shipping) {
+                startActivity(new Intent(this, Shipping.class));
+            }
+            finish();
+        }, 300);
+        return true;
+    }
+}
