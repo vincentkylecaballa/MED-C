da854d229c889a2d4557d9fd8f1ed14b62e9afad
Vincent Kyle Caballa <vincentkyle27@gmail.com>
2021-02-14 12:42:09 +0800

    changes

---
+++ app/src/main/java/com/example/myapplication/product_details.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class product_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_details);
+    }
+}
